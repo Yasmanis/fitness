7ce2dedb3b75971129a49af9f8f5692fea7f1306
Yasmanis Basulto <yasmabc@gmail.com>
2019-09-05 19:44:21 -0400

    lossdd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            UserFichasSeeder::class,
             HistoryMedicalDescriptionSeeder::class,
             MedicalHistorySeeder::class,
             ConditionSeeder::class,
